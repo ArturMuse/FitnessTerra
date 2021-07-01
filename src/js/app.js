4ba4982fdee3217f7a0a940bc0f206cbe110b49d
Arthur Barski <82526308+ArturMuse@users.noreply.github.com>
2021-07-01 17:57:23 +0300

    Update popup

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import '../scss/app.scss';
 import "./main/sales";
 import Popup from "./popup";
 
+// Contact Form Popup Init
 const contactForm = new Popup(".js-popup-contact", ".js-popup-contact-toggle", "popup-show", ".js-popup-contact-close")
 contactForm.popupInit()
 
--- src/js/popup.js
+++ src/js/popup.js
@@ -4,15 +4,18 @@ class Popup {
         this.popupToggles = document.querySelectorAll(toggles)
         this.popupShow = popupShow
         this.popupCloseBtn = document.querySelector(popupCloseBtn)
+        this.body = document.querySelector("body")
     }
     popupInit() {
         this.popupToggles.forEach(el => {
             el.addEventListener("click", () => {
                 this.popup.classList.toggle(this.popupShow)
+                this.body.style.overflow = "hidden"
             })
         })
         this.popupCloseBtn.addEventListener("click", () => {
             this.popup.classList.toggle(this.popupShow)
+            this.body.style.overflow = "auto"
         })
     }
 }
